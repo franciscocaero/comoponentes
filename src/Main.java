dceadb07918e25063c2db8d5c5c632ac5d991c19
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-01-19 12:45:12 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame=new JFrame("Frame ");
+        frame.setContentPane(new form1().menuPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,600);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
